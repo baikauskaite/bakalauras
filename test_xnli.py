dba10f05460958b5f7b9fb58a167b15afe047b77
Viktorija Baikauskaitė <baikauskaitev@gmail.com>
2024-04-20 18:25:11 +0300

    Update XNLI for debugging

--- test_xnli.py
+++ test_xnli.py
@@ -1,5 +1,5 @@
 from transformers import AutoModelForSequenceClassification, AutoTokenizer, Trainer, TrainingArguments
-from datasets import load_dataset
+from datasets import load_dataset, Dataset, DatasetDict
 import os
 import evaluate
 import numpy as np
@@ -16,45 +16,63 @@ MODEL_SELECTION = SOURCE_MODEL_DIR
 
 model_name = MODEL_SELECTION.split("/")[-1]
 batch_size = 8
+train_size = 1_000
 
 # ##############################################################################
 
 def load_model_and_tokenizer(model_name, num_labels):
-    model = AutoModelForSequenceClassification.from_pretrained(model_name, num_labels=num_labels)
+    model = AutoModelForSequenceClassification.from_pretrained(model_name, num_labels=num_labels, output_hidden_states=False,output_attentions=False)
     tokenizer = AutoTokenizer.from_pretrained(model_name)
     return model, tokenizer
 
-def prepare_data(tokenizer, dataset):
+def prepare_data(tokenizer, dataset: Dataset | DatasetDict):
     def tokenize(examples):
-        return tokenizer(examples['premise'], examples['hypothesis'], padding=True, truncation=True, return_tensors='pt', max_length=256)
+        return tokenizer(examples['premise'], examples['hypothesis'], truncation=True, padding=True, return_tensors='pt', max_length=256)
+    
     dataset = dataset.map(tokenize, batched=True)
-    dataset.set_format(type='torch', columns=['input_ids', 'attention_mask', 'label'])
+
+    if 'token_type_ids' in dataset.column_names:
+        dataset.set_format(type='torch', columns=['input_ids', 'attention_mask', 'token_type_ids', 'labels'])
+    else:
+        dataset.set_format(type='torch', columns=['input_ids', 'attention_mask', 'labels'])
+        
     return dataset
 
 def compute_metrics(eval_pred):
     logits, labels = eval_pred
     predictions = np.argmax(logits, axis=-1)
-    return metric.compute(predictions=predictions, references=labels)
+    return xnli_metric.compute(predictions=predictions, references=labels)
+
+class DebugTrainer(Trainer):
+    def prediction_step(self, model, inputs, prediction_loss_only, ignore_keys=None):
+        outputs = super().prediction_step(model, inputs, prediction_loss_only=False, ignore_keys=None)
+        _, logits, labels = outputs
+        predictions = np.argmax(logits, axis=-1)
+        print("Predictions:", predictions)
+        print("Labels:", labels)
+        return outputs
 
 # ##############################################################################
 
 # Load XNLI dataset
-metric = evaluate.load("accuracy")
+xnli_metric = evaluate.load("xnli")
 dataset = load_dataset("xnli", "fr")
 
-train_size = 1_000
 test_size = int(0.1 * train_size)
 
 downsampled_dataset = dataset["train"].train_test_split(
     train_size=train_size, test_size=test_size, seed=42
 )
-print(downsampled_dataset['train'][:4])
 
 num_labels = len(set(downsampled_dataset['train']['label']))
-print(f"Number of labels: {num_labels}")
 model, tokenizer = load_model_and_tokenizer(MODEL_SELECTION, num_labels)
 
 # Prepare data for training and evaluation
+downsampled_dataset = downsampled_dataset.map(lambda example: {
+    'labels': example['label'], 
+    **{k: v for k, v in example.items() if k != 'label'}
+    }, remove_columns=['label'])
+
 train_dataset = prepare_data(tokenizer, downsampled_dataset['train'])
 eval_dataset = prepare_data(tokenizer, downsampled_dataset['test'])
 
@@ -76,12 +94,12 @@ training_args = TrainingArguments(
 )
 
 # Initialize the Trainer
-trainer = Trainer(
+trainer = DebugTrainer(
     model=model,
     args=training_args,
     train_dataset=train_dataset,
     eval_dataset=eval_dataset,
-    # compute_metrics=compute_metrics,
+    compute_metrics=compute_metrics,
 )
 
 # Train the model
@@ -89,4 +107,3 @@ trainer.train()
 
 # Evaluate the model
 results = trainer.evaluate()
-# print(f"Accuracy on XNLI task: {results['eval_accuracy']}") nera tokio eval_accuracy
