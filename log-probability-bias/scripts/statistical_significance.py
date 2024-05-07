b57e4a17f9f2b6d9c3f9827272c231325493d411
Viktorija Baikauskaitė <baikauskaitev@gmail.com>
2024-05-07 19:29:27 +0300

    Count effect size in log probability

--- log-probability-bias/scripts/statistical_significance.py
+++ log-probability-bias/scripts/statistical_significance.py
@@ -3,6 +3,20 @@ import numpy as np
 from scipy import stats
 import pandas as pd
 
+def get_effect_size(df1, df2, k="log_probs"):
+    diff = (df1[k].mean() - df2[k].mean())
+    std_ = pd.concat([df1, df2], axis=0)[k].std() + 1e-8
+    return diff / std_
+
+def exact_mc_perm_test(xs, ys, nmc=100000):
+    n, k = len(xs), 0
+    diff = np.abs(np.mean(xs) - np.mean(ys))
+    zs = np.concatenate([xs, ys])
+    for j in range(nmc):
+        np.random.shuffle(zs)
+        k += diff < np.abs(np.mean(zs[:n]) - np.mean(zs[n:]))
+    return k / nmc
+
 for fpath in sys.argv[1:]:
     df = pd.read_csv(fpath, sep='\t')
     attributes = np.unique(df.attribute)
@@ -25,3 +39,8 @@ for fpath in sys.argv[1:]:
         print("Test statistic,\t", wilcoxon[0])
         print("p-value,\t", wilcoxon[1])
         print()
+
+    attr1 = df[df.attribute == attributes[0]]
+    attr2 = df[df.attribute == attributes[1]]
+    print("Effect size,\t", get_effect_size(attr1, attr2))
+    print("Permutation test p-value,\t", exact_mc_perm_test(attr1.log_probs, attr2.log_probs))
